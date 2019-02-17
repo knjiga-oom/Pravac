f045e51e5a5d3ee32bbcd24a88255006ddb10fae
Julijan Šribar <julijan.sribar@gmail.com>
2019-02-17 23:31:22 +0100

    Dodana OO verzija-

---
+++ OOVerzija/Pravac.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOVerzija
+{
+    class Pravac
+    {
+        public Pravac(double a, double b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public IEnumerable<Točka> Presjecište(Pravac p)
+        {
+            List<Točka> rezultat = new List<Točka>();
+            if (a != p.a)
+            {
+                double x = (p.b - b) / (a - p.a);
+                double y = a * x + b;
+                rezultat.Add(new Točka(x, y));
+            }
+            return rezultat;
+        }
+
+        private double a;
+        private double b;
+    }
+}
