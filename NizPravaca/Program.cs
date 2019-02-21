e39fbe775a0249542c43f66364b886c9b695c289
Julijan Šribar <julijan.sribar@gmail.com>
2019-02-21 21:52:25 +0100

    Promijenjene oznake koeficijenata.

--- NizPravaca/Program.cs
+++ NizPravaca/Program.cs
@@ -10,21 +10,24 @@ namespace NizPravaca
     {
         static void Main(string[] args)
         {
-            List<double> a = new List<double> { 1.0, -1.0, 2.0 };
-            List<double> b = new List<double> { 0.0, 2.0, -3.0 };
+            List<double> k = new List<double> { 1.0, -1.0, 2.0 };
+            List<double> l = new List<double> { 0.0, 2.0, -3.0 };
 
-            Sjecište(a[0], b[0], a[2], b[2]);
+            Sjecište(k[0], l[0], k[2], l[2]);
+            Sjecište(k[1], l[1], k[2], l[2]);
+
+            Console.ReadKey(true);
         }
 
-        static void Sjecište(double a1, double b1, double a2, double b2)
+        static void Sjecište(double k1, double l1, double k2, double l2)
         {
-            if (a1 == a2)
+            if (k1 == k2)
                 Console.WriteLine("Pravci su paralelni");
             else
             {
-                double x = (b2 - b1) / (a1 - a2);
-                double y = a1 * x + b1;
-                Console.WriteLine($"Presjecište: ({x}, {y})");
+                double x = (l2 - l1) / (k1 - k2);
+                double y = k1 * x + l1;
+                Console.WriteLine($"Sjecište: ({x}, {y})");
             }
         }
     }
